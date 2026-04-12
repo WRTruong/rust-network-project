3f2c241976b1105ab27a150e6d96ff5cb698d7b1
VoQuocTri <htltri9@gmail.com>
2026-04-12 16:12:22 +0700

    [feat] add multi-room chat UI and improve message handling

--- src/client/client.rs
+++ src/client/client.rs
@@ -6,7 +6,7 @@ use tokio_tungstenite::{connect_async, tungstenite::Message};
 
 pub async fn start_client() -> Result<(), Box<dyn std::error::Error>> {
     let username = read_input("Enter username: ")?;
-
+    let room = "general".to_string();
     let server_url = "ws://127.0.0.1:3000/ws";
     let (socket, response) = connect_async(server_url).await?;
     let (mut write, mut read) = socket.split();
@@ -15,29 +15,38 @@ pub async fn start_client() -> Result<(), Box<dyn std::error::Error>> {
     println!("Handshake status: {}", response.status());
     println!("Type messages and press Enter. Type /quit to exit.");
 
+    let join_msg = ChatMessage {
+        msg_type: "join".into(),
+        username: username.clone(),
+        content: "".into(),
+        room: room.clone(),
+    };
+
+    write
+        .send(Message::Text(serde_json::to_string(&join_msg)?.into()))
+        .await?;
+
     let receive_task = tokio::spawn(async move {
         while let Some(frame) = read.next().await {
             match frame {
                 Ok(Message::Text(text)) => {
                     match serde_json::from_str::<ChatMessage>(&text) {
-                        Ok(received_message) => {
-                            println!(
-                                "{}: {}",
-                                received_message.username, received_message.content
-                            );
+                        Ok(msg) => {
+                            if msg.msg_type == "system" {
+                                println!("[SYSTEM] {}", msg.content);
+                            } else {
+                                println!("{}: {}", msg.username, msg.content);
+                            }
                         }
-                        Err(error) => eprintln!("Invalid message from server: {}", error),
+                        Err(e) => eprintln!("Parse error: {}", e),
                     }
                 }
-                Ok(Message::Close(close_frame)) => {
-                    println!("Server closed connection: {:?}", close_frame);
+                Ok(Message::Close(_)) => break,
+                Err(e) => {
+                    eprintln!("Receive error: {}", e);
                     break;
                 }
-                Ok(_) => {}
-                Err(error) => {
-                    eprintln!("Receive error: {}", error);
-                    break;
-                }
+                _ => {}
             }
         }
     });
@@ -54,17 +63,31 @@ pub async fn start_client() -> Result<(), Box<dyn std::error::Error>> {
             continue;
         }
 
-        let message: ChatMessage = ChatMessage {
+        let msg = ChatMessage {
+            msg_type: "message".into(),
             username: username.clone(),
             content,
+            room: room.clone(),
         };
-        let serialized_message = serde_json::to_string(&message)?;
 
-        if write.send(Message::Text(serialized_message.into())).await.is_err() {
+        let serialized = serde_json::to_string(&msg)?;
+        
+        if write.send(Message::Text(serialized.into())).await.is_err() {
             println!("Disconnected from server");
             break;
         }
     }
+
+    let leave_msg: ChatMessage = ChatMessage {
+        msg_type: "leave".into(),
+        username: username.clone(),
+        content: "".into(),
+        room: room.clone(),
+    };
+
+    let _ = write
+        .send(Message::Text(serde_json::to_string(&leave_msg)?.into()))
+        .await;
 
     let _ = write.close().await;
     let _ = receive_task.await;
